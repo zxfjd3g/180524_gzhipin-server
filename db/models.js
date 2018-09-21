59d1c9f48b1fd03ad4fe69f9a2891f1a9e15efcc
zxfjd3g <258147149@qq.com>
2018-09-21 14:22:16 +0800

    update

--- db/models.js
+++ db/models.js
@@ -18,7 +18,7 @@
 // 1.1. 引入mongoose
 const mongoose = require('mongoose')
 // 1.2. 连接指定数据库(URL只有数据库是变化的)
-mongoose.connect('mongodb://192.168.17.25:27017/gzhipin5')
+mongoose.connect('mongodb://localhost:27017/gzhipin5')
 // 1.3. 获取连接对象
 const conn = mongoose.connection
 // 1.4. 绑定连接完成的监听(用来提示连接成功)
